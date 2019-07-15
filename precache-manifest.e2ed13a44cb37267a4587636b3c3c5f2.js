b54a673de33a15489730e4628f87187a3214c433
Jorge Contreras <jorge.contreras789@gmail.com>
2019-07-15 16:05:15 -0400

    Updates

---
+++ precache-manifest.e2ed13a44cb37267a4587636b3c3c5f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "86eae0b0d25efab82676",
+    "url": "/robofriends/static/css/main.7e40c8e9.chunk.css"
+  },
+  {
+    "revision": "86eae0b0d25efab82676",
+    "url": "/robofriends/static/js/main.d3915e36.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "a9e2f286e25a8c0008c2",
+    "url": "/robofriends/static/css/2.a8446ef0.chunk.css"
+  },
+  {
+    "revision": "a9e2f286e25a8c0008c2",
+    "url": "/robofriends/static/js/2.223275fe.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "11fecd86637005b268425a181d3498b4",
+    "url": "/robofriends/index.html"
+  }
+];
