3e5257be883d9bb67c751acbc84a5d39aece6f3f
Jorge Contreras <jorge.contreras789@gmail.com>
2019-07-17 12:24:05 -0400

    created a main page component and a test for it, created a test for actions, created a test for reducers

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setSearchField, requestRobots } from '../actions';
 
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
-import Header from '../components/Header';
+import MainPage from '../components/MainPage';
 
-import './App.css';
+// import './App.css';
 
 const mapStateToProps = state => {
   return {
@@ -27,28 +23,8 @@ const mapDispatchToProps = dispatch => {
 };
 
 class App extends Component {
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
-
   render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-    return isPending ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className="tc">
-        <Header />
-        <SearchBox searchChange={onSearchChange} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
+    return <MainPage {...this.props} />;
   }
 }
 
